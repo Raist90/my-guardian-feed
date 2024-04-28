4c59f41c35652ba5c015e004deda4c513c20431a
Jaromil90 <manuel.detoma@noaignite.com>
2024-04-28 11:21:53 +0200

    tweak getGuardianData function

--- pages/index/+data.ts
+++ pages/index/+data.ts
@@ -1,5 +1,13 @@
 import { getGuardianData } from '@/helpers/getGuardianData'
 
-export const data = async () => {
-  return await getGuardianData()
+/**
+ * @todo All options params will be passed from clientside. We maybe need to
+ *   inject some initial state because of index page
+ */
+export async function data() {
+  return await getGuardianData({
+    page: 1,
+    query: 'europe AND italy',
+    section: 'world|politic|economic|culture',
+  })
 }
--- src/helpers/getGuardianData.ts
+++ src/helpers/getGuardianData.ts
@@ -1,14 +1,24 @@
 import type { GuardianAPIData } from '@/types'
 import fetch from 'cross-fetch'
 
+type Options = {
+  page: number
+  query: string
+  section: string
+}
+
 /**
  * Use this only serverside in order to not expose your api key
  *
  * @todo Make sure to complete this one
  */
-export async function getGuardianData() {
+export async function getGuardianData(options: Options) {
+  const page = options.page
+  const query = options.query
+  const section = options.section
+
   const response = await fetch(
-    `${import.meta.env.GUARDIAN_API}/search?q=italy AND sweden&show-fields=trailText,thumbnail&show-tags=all&section=world|politic|economic|culture&order-by=newest&api-key=${import.meta.env.GUARDIAN_API_KEY}`,
+    `${import.meta.env.GUARDIAN_API}/search?q=${query}&show-fields=trailText,thumbnail&show-tags=all&section=${section}&order-by=newest&page=${page}&api-key=${import.meta.env.GUARDIAN_API_KEY}`,
   )
 
   const guardianData: Awaited<GuardianAPIData> = await response.json()
