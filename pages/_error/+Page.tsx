7d7a0b6ae91823a8d88f3f90b3efcfd165680bbe
Jaromil90 <manuel.detoma@noaignite.com>
2024-05-12 17:30:04 +0200

    delete unused routes and tweak error page styles

--- pages/_error/+Page.tsx
+++ pages/_error/+Page.tsx
@@ -6,21 +6,21 @@ export default function Page() {
   const { abortReason, is404 } = usePageContext()
   if (is404) {
     return (
-      <>
+      <div className='p-4'>
         <h1>404 Page Not Found</h1>
         <p>This page could not be found.</p>
-      </>
+      </div>
     )
   } else {
     return (
-      <>
+      <div className='p-4'>
         <h1>500 Internal Server Error</h1>
         {isDevelopment && abortReason ? (
           <p>{String(abortReason)}</p>
         ) : (
           <p>Something went wrong.</p>
         )}
-      </>
+      </div>
     )
   }
 }
