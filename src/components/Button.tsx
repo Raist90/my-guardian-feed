1eda2adafabc3f61ecd3b85e803e9eeecb4c7126
Jaromil90 <manuel.detoma@noaignite.com>
2024-07-11 21:35:09 +0200

    tweak Button component props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,20 +2,23 @@ export { Button }
 
 import { capitalize } from '@/helpers/capitalize'
 import { useCustomFeed } from '@/hooks/useCustomFeed'
+import clsx from 'clsx'
 import React from 'react'
 
 type ButtonProps =
   | {
-      isButtonGroup: true
-      buttonGroup: string[]
-      isDisabled: boolean
-      handlers: Record<string, () => void>
-    }
+    isButtonGroup: true
+    buttonGroup: string[]
+    isDisabled: boolean
+    handlers: Record<string, () => void>
+  }
   | {
-      children: React.ReactNode | string
-      isDisabled?: boolean
-      handler: () => void
-    }
+    children: React.ReactNode | string
+    className?: string
+    isDisabled?: boolean
+    handler?: () => void
+    type?: HTMLButtonElement['type']
+  }
 
 function Button(props: ButtonProps) {
   const customFeed = useCustomFeed()
@@ -37,13 +40,15 @@ function Button(props: ButtonProps) {
     )
   }
 
-  const { children, isDisabled, handler } = props
+  const { children, isDisabled, handler, type = 'button', ...rest } = props
+  const className = rest.className
 
   return (
     <button
-      className='border p-2 disabled:opacity-75'
+      className={clsx(className && className, 'border p-2 disabled:opacity-75')}
       disabled={isDisabled || false}
       onClick={handler}
+      type={type}
     >
       {children}
     </button>
