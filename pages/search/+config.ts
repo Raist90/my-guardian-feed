5aa2fb7b6b70098bc8f844f6374f4aa567f3b56b
Jaromil90 <manuel.detoma@noaignite.com>
2024-07-15 21:45:45 +0200

    fix: remove doubled layout in search route

--- pages/search/+config.ts
+++ pages/search/+config.ts
@@ -1,11 +1,9 @@
 import { SITE_TITLE } from '@/constants'
 import Head from '@/layouts/HeadDefault.jsx'
-import Layout from '@/layouts/LayoutDefault'
 import vikeReact from 'vike-react/config'
 import type { Config } from 'vike/types'
 
 export default {
-  Layout,
   Head,
   title: `Search | ${SITE_TITLE}`,
   extends: vikeReact,
