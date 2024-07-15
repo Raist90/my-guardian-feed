ae89d6576fa06a7fbbeeb21626ed728fab448ff1
Jaromil90 <manuel.detoma@noaignite.com>
2024-07-15 21:41:08 +0200

    add userFeedsTable in drizzle schema.ts

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,4 +1,4 @@
-export { usersTable }
+export { userFeedsTable, usersTable }
 
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
@@ -6,4 +6,15 @@ const usersTable = sqliteTable('users', {
   id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
   email: text('email'),
   password: text('password'),
+})
+
+const userFeedsTable = sqliteTable('userFeeds', {
+  favorite: text('favorite'),
+  feedURL: text('feed_url'),
+  history: text('history'),
+  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  lists: text('lists'),
+  readLater: text('read_later'),
+  suggestedNews: text('suggested_news'),
+  userID: integer('user_id').references(() => usersTable.id),
 })
