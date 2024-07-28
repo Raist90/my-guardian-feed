045b1d7c14f45d02f1d4b0b96dceb0815bc71a7e
Jaromil90 <manuel.detoma@noaignite.com>
2024-07-28 21:33:32 +0200

    fix redundant option in pages/news/+config.ts

--- pages/news/+config.ts
+++ pages/news/+config.ts
@@ -1,10 +1,8 @@
 import Head from '@/layouts/HeadDefault.jsx'
-import Layout from '@/layouts/LayoutDefault'
 import vikeReact from 'vike-react/config'
 import type { Config } from 'vike/types'
 
 export default {
-  Layout,
   Head,
   extends: vikeReact,
 } satisfies Config
